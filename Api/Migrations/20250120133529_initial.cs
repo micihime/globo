f646147447863322640b6e6ef771f04e11824477
Miroslava Hreskova <hroncovam@yahoo.com>
2025-01-20 14:36:33 +0100

    Adding initial migration - dotnet ef migrations add initial

---
+++ Api/Migrations/20250120133529_initial.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Houses",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Address = table.Column<string>(type: "TEXT", nullable: true),
+                    Country = table.Column<string>(type: "TEXT", nullable: true),
+                    Description = table.Column<string>(type: "TEXT", nullable: true),
+                    Price = table.Column<int>(type: "INTEGER", nullable: false),
+                    Photo = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Houses", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Houses",
+                columns: new[] { "Id", "Address", "Country", "Description", "Photo", "Price" },
+                values: new object[,]
+                {
+                    { 1, "1234 Main St", "USA", "A nice home in the suburbs", null, 120000 },
+                    { 2, "5678 Elm St", "USA", "A cozy home in the city", null, 200000 },
+                    { 3, "9012 Oak St", "USA", "A spacious home in the countryside", null, 300000 }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Houses");
+        }
+    }
+}
