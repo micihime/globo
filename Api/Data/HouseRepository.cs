1ef947fdd1b23f1432c6b1649d77a6acb8bfbd91
Miroslava Hreskova <hroncovam@yahoo.com>
2025-01-20 17:46:57 +0100

    Creating HouseRepository

---
+++ Api/Data/HouseRepository.cs
@@ -0,0 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+
+public class HouseRepository
+{
+    private readonly HouseDbContext context;
+
+    public HouseRepository(HouseDbContext context)
+    {
+        this.context = context;
+    }
+
+    public async Task<List<HouseDto>> GetAll()
+    {
+        return await context.Houses.Select(h => new HouseDto(h.Id, h.Address, h.Country, h.Price)).ToListAsync();
+    }
+}
