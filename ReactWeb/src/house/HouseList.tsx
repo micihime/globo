88d968c5a65956ac157e728af5ee5c681c51da69
Miroslava Hreskova <hroncovam@yahoo.com>
2025-01-21 14:48:21 +0100

    Creating custom hook to fetch houses

--- ReactWeb/src/house/HouseList.tsx
+++ ReactWeb/src/house/HouseList.tsx
@@ -1,18 +1,8 @@
-import { useState } from "react";
-import { House } from "../types/house";
-import config from "../config";
+import useFetchHouses from "../hooks/HouseHooks";
 
 const HouseList = () => {
-    const [houses, setHouses] = useState<House[]>([]);
-
-    const fetchHouses = async () => {
-        const response = await fetch(`${config.baseApiUrl}/houses`);
-        const houses = await response.json();
-        setHouses(houses);
-    }
-
-    fetchHouses();
-
+    const houses= useFetchHouses();
+    
     return (
         <div>
             <div className="row mb-2">
