9cf3ab056cef684d12f64eeabb5fd6f49a16f903
Miroslava Hreskova <hroncovam@yahoo.com>
2025-01-22 15:51:28 +0100

    Getting data + error handling added

--- ReactWeb/src/house/HouseDetail.tsx
+++ ReactWeb/src/house/HouseDetail.tsx
@@ -1,12 +1,21 @@
 import { useParams } from "react-router-dom";
+import { useFetchHouse } from "../hooks/HouseHooks";
+import ApiStatus from "../apiStatus";
 
 const HouseDetail = () => {
     const { id } = useParams();
     if (!id) throw Error("House id is missing in the route params");
     const houseId = parseInt(id);
+    if (isNaN(houseId)) throw Error("House id is not a number");
+
+    const {data, status, isSuccess} = useFetchHouse(houseId);
+    if (!isSuccess) return <ApiStatus status={status} />
+    if (!data) return <div>House not found</div>
 
     return (
-        <div>House Detail {houseId}</div>
+        <div className="row">
+            House Detail - {data.description}
+        </div>
     )
 }
 
