e4b3d9fa5d57d24a9ce337cff88b851054042325
Miroslava Hreskova <hroncovam@yahoo.com>
2025-01-22 15:47:04 +0100

    Add reading id from URL params

--- ReactWeb/src/house/HouseDetail.tsx
+++ ReactWeb/src/house/HouseDetail.tsx
@@ -1,6 +1,12 @@
+import { useParams } from "react-router-dom";
+
 const HouseDetail = () => {
+    const { id } = useParams();
+    if (!id) throw Error("House id is missing in the route params");
+    const houseId = parseInt(id);
+
     return (
-        <div>House Detail</div>
+        <div>House Detail {houseId}</div>
     )
 }
 
