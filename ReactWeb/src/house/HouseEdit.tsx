ebd66ee4859220ec0a52129076df6af588f6464b
Miroslava Hreskova <hroncovam@yahoo.com>
2025-01-26 22:28:40 +0100

    Adding HouseEdit component

---
+++ ReactWeb/src/house/HouseEdit.tsx
@@ -0,0 +1,24 @@
+import { useParams } from "react-router-dom";
+import { useFetchHouse, useUpdateHouse } from "../hooks/HouseHooks";
+import ApiStatus from "../apiStatus";
+import HouseForm from "./HouseForm";
+
+const HouseEdit = () => {
+  const { id } = useParams();
+  if (!id) throw Error("House id not found");
+  const houseId = parseInt(id);
+
+  const { data, status, isSuccess } = useFetchHouse(houseId);
+  const updateHouseMutation = useUpdateHouse();
+
+  if (!isSuccess) return <ApiStatus status={status} />;
+
+  return (
+    <HouseForm 
+      house={data}
+      submitted={(house) => updateHouseMutation.mutate(house)}
+    />
+  )
+};
+
+export default HouseEdit;
