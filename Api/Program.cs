5479c8760b42491a426f9fc26df9947f6f4ccfed
Miroslava Hreskova <hroncovam@yahoo.com>
2025-02-10 21:44:02 +0100

    Refactoring of endpoints - cleanup

--- Api/Program.cs
+++ Api/Program.cs
@@ -29,61 +29,7 @@ app.UseCors(p => p.WithOrigins("http://localhost:3000").AllowAnyMethod().AllowAn
 
 app.UseHttpsRedirection();
 
-app.MapGet("/houses", (IHouseRepository repo) => repo.GetAll())
-    .Produces<HouseDto[]>(StatusCodes.Status200OK);
-
-app.MapGet("/houses/{houseId:int}", async (int houseId, IHouseRepository repo) =>
-{
-    var house = await repo.Get(houseId);
-    if (house == null)
-        return Results.Problem($"House {houseId} not found", statusCode: 404);
-    return Results.Ok(house);
-}).ProducesProblem(404).Produces<HouseDetailDto>();
-
-app.MapPost("/houses", async ([FromBody] HouseDetailDto dto, IHouseRepository repo) =>
-{
-    if (!MiniValidator.TryValidate(dto, out var errors))
-        return Results.ValidationProblem(errors);
-    var newHouse = await repo.Add(dto);
-    return Results.Created($"/houses/{newHouse.Id}", newHouse);
-}).Produces<HouseDetailDto>(StatusCodes.Status201Created)
-    .ProducesValidationProblem();
-
-app.MapPut("/houses", async ([FromBody] HouseDetailDto dto, IHouseRepository repo) =>
-{
-    if (!MiniValidator.TryValidate(dto, out var errors))
-        return Results.ValidationProblem(errors);
-    if (await repo.Get(dto.Id) == null)
-        return Results.Problem($"House {dto.Id} not found", statusCode: 404);
-    var updatedHouse = await repo.Update(dto);
-    return Results.Ok(updatedHouse);
-}).ProducesProblem(404).Produces<HouseDetailDto>()
-    .ProducesValidationProblem();
-
-app.MapDelete("/houses/{houseId:int}", async (int houseId, IHouseRepository repo) =>
-{
-    if (await repo.Get(houseId) == null)
-        return Results.Problem($"House {houseId} not found", statusCode: 404);
-    await repo.Delete(houseId);
-    return Results.Ok();
-}).ProducesProblem(404).Produces(StatusCodes.Status200OK);
-
-app.MapGet("house/{houseId:int}/bids", async (int houseId, IHouseRepository houseRepo, IBidRepository bidRepo) =>
-{
-    if (await houseRepo.Get(houseId) == null)
-        return Results.Problem($"House {houseId} not found", statusCode: 404);
-    var bids = await bidRepo.Get(houseId);
-    return Results.Ok(bids);
-}).ProducesProblem(404).Produces(StatusCodes.Status200OK);
-
-app.MapPost("house/{houseId:int}/bids", async (int houseId, [FromBodyAttribute] BidDto dto, IBidRepository repo) =>
-{
-    if (dto.HouseId != houseId)
-        return Results.Problem("No match", statusCode: StatusCodes.Status400BadRequest);
-    if (!MiniValidator.TryValidate(dto, out var errors))
-        return Results.ValidationProblem(errors);
-    var newBid = await repo.Add(dto);
-    return Results.Created($"/houses/{newBid.HouseId}/bids", newBid);
-}).ProducesValidationProblem().ProducesProblem(400).Produces<BidDto>(StatusCodes.Status201Created);
+app.MapHouseEndpoints();
+app.MapBidEndpoints();
 
 app.Run();
